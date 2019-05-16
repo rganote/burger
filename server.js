5ca45a74984f2da23b222cfda33df62e54b1ccb8
rganote <46900442+rganote@users.noreply.github.com>
2019-05-15 23:42:14 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+//display static content from "public" directory in the application directory
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burger_controllers");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("main server.js listening on: http://localhost:" + PORT);
+});
